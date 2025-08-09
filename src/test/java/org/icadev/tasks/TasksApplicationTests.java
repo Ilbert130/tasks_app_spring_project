063bb8b8fb3b9cc2e304c264ea8235bfde5464ea
Ilbert130 <ilbertca27@gmail.com>
2025-08-09 12:15:37 -0400

    Initial commit

---
+++ src/test/java/org/icadev/tasks/TasksApplicationTests.java
@@ -0,0 +1,13 @@
+package org.icadev.tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
